45e232080a03eecbd6840246be7e48dfbe30b6bd
hexagramme <hibougalante@proton.me>
2023-08-07 19:13:26 -0700

    Added product definition and controller with POST mapping

---
+++ shoppy/src/main/java/shoppy/hexagramme/com/shoppy/Product.java
@@ -0,0 +1,8 @@
+package shoppy.hexagramme.com.shoppy;
+
+public class Product {
+    long id;
+    String name;
+    double price;
+    String description; 
+}
---
+++ shoppy/src/main/java/shoppy/hexagramme/com/shoppy/ProductController.java
@@ -0,0 +1,25 @@
+package shoppy.hexagramme.com.shoppy;
+
+import java.util.concurrent.atomic.AtomicLong;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class ProductController {
+    private static final String defaultDescription = "No description currenlty available for this item.";
+    private final AtomicLong counter = new AtomicLong();
+    private static final String defaultPrice = "9.99";
+    private static final String defaultName = "Dummy Item";
+
+    @PostMapping("/add")
+    public Product newProduct(
+        @RequestParam(value = "name", defaultValue = defaultName ) String name,
+        @RequestParam(value = "price", defaultValue = defaultPrice ) double price,
+        @RequestParam(value = "description", defaultValue = defaultDescription) String description,
+         ) {
+            return new Product(counter.incrementAndGet(), name, price, description);
+        }
+}
